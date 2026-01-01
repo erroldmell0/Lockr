affe7b82fd5cc512758bf3af3c432711ebc24e58
Errol D'mello <erroldmello2005@gmail.com>
2026-01-01 21:23:40 +0530

    Made the website responsive

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -42,10 +42,34 @@ const Manager = () => {
   }
 
   const savePassword = () => {
-    setPasswordArray([...passwordArray, {...form, id: uuidv4()}])
-    setForm({site: "", username: "", password: ""})
-    localStorage.setItem("passwords", JSON.stringify([...passwordArray, {...form, id: uuidv4()}])) // because it takes time for states to update
-    //console.log([...passwordArray, form])
+    if(form.site.length>=3 && form.username.length>=3 && form.password.length>=3) {
+      setPasswordArray([...passwordArray, {...form, id: uuidv4()}])
+      setForm({site: "", username: "", password: ""})
+      localStorage.setItem("passwords", JSON.stringify([...passwordArray, {...form, id: uuidv4()}])) // because it takes time for states to update
+      toast('Password Saved Successfully', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+    } else {
+      toast('Password Not Saved', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
+    }
   }
 
   const editPassword = (id) => {
@@ -58,6 +82,18 @@ const Manager = () => {
     if(confirm("Are you sure you want to delete this password?")) {
       setPasswordArray(passwordArray.filter(item=>item.id!==id))
       localStorage.setItem("passwords", JSON.stringify(passwordArray.filter(item=>item.id!==id)))
+
+      toast('Password deleted successfully', {
+        position: "top-right",
+        autoClose: 1000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+        transition: Bounce,
+      });
       console.log("password deleted")
     }
   }
@@ -80,22 +116,22 @@ const Manager = () => {
       />
 
       {/*bg ibelick */}
-      <div className="absolute inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(200%_250%_at_50%_10%,#fff_10%,#32cd32_120%)]"></div>
+      <div className="fixed inset-0 -z-10 h-full w-full bg-white [background:radial-gradient(200%_250%_at_50%_10%,#fff_10%,#32cd32_120%)]"></div>
 
-      <div className=" mycontainer">
+      <div className="p-2 pt-16 md:mycontainer max-w-4xl mx-auto ">
         <h1 className='text-4xl font-bold text-center'>
-          <span className='text-green-500'>&lt;</span>
+          <span className='text-green-500 '>&lt;</span>
           Lockr
           <span className='text-green-500'>/&gt;</span>
         </h1>
         <p className='text-green-900 tex-lg text-center'>Your Own Password Manager</p>
 
         <div className='text-black flex flex-col p-4 gap-8 items-center'>
-          <input value={form.site} onChange={handleChange} placeholder='Enter Website URL' className='rounded-full border border-green-500 w-full px-4 py-1' type='text' name='site' id=''/>
-          <div className="flex w-full justify-between gap-8">
-            <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full px-4 py-1' type='text' name='username' id=''/>
+          <input value={form.site} onChange={handleChange} placeholder='Enter Website URL' className='rounded-full border border-green-500 w-full px-4 py-1' type='text' name='site' id='site'/>
+          <div className="flex flex-col md:flex-row w-full justify-between gap-8">
+            <input value={form.username} onChange={handleChange} placeholder='Enter Username' className='rounded-full border border-green-500 w-full px-4 py-1' type='text' name='username' id='username'/>
             <div className='relative'>
-              <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter password' className='rounded-full border border-green-500 w-full px-4 py-1' type='password' name='password' id=''/>
+              <input ref={passwordRef} value={form.password} onChange={handleChange} placeholder='Enter password' className='rounded-full border border-green-500 w-full px-4 py-1' type='password' name='password' id='password'/>
               <span className='absolute right-[3px] top-1 cursor-pointer' onClick={showpassword}>
                 <img ref={ref} className='p-1' width={26} src="icons/eyecross.png" />
               </span>
@@ -116,7 +152,7 @@ const Manager = () => {
           <h2 className='font-bold text-xl m-2'>Your Passwords</h2>
           {passwordArray.length === 0 && <div> No passwords to show</div>}
           {passwordArray.length != 0 &&
-            <table className="table-fixed w-full rounded-md overflow-hidden">
+            <table className="table-fixed w-full rounded-md overflow-hidden mb-22">
               <thead className='bg-green-800 text-white'>
                 <tr>
                   <th className='py-2'>Website</th>
@@ -128,8 +164,8 @@ const Manager = () => {
               <tbody className='bg-green-100'>
                 {passwordArray.map((item, index) => {
                   return <tr key={index}>
-                    <td className='py-2 border border-white min-w-32'>
-                      <div className='flex items-center justify-center'>
+                    <td className='py-2 border border-white min-w-32 overflow-scroll'>
+                      <div className='flex items-center justify-center '>
                         <a href={item.site} target='_blank'>{item.site}</a>
                         <div className="cursor-pointer" onClick={()=>{copyText(item.site)}}>
                           <lord-icon
